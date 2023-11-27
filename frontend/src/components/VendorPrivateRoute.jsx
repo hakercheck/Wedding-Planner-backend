04389679e4dad79802800c2ee99161b9e9ac93ee
Pratyush7044 <pratyush.manna2021@vitstudent.ac.in>
2023-11-28 00:14:54 +0530

    h

--- frontend/src/components/VendorPrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-const VendorPrivateRoute = () => {
-  const { userInfo } = useSelector((state) => state.auth);
-  return (userInfo.role === 'vendor') ? <Outlet /> : <Navigate to='/login' replace />;
-};
-export default VendorPrivateRoute;
